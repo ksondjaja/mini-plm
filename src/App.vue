6a83d550e58fa3a140a3f1d3fd1eed6642b4d231
ksondjaja <sondjajaja@gmail.com>
2023-09-09 20:32:38 -0400

    prevent unauthorized users

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
-  <nav>
+  <nav v-if="$store.state.user">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <button @click="$store.dispatch('logout')">Logout</button>
   </nav>
   <router-view/>
 </template>
+
+<script>
+import { onBeforeMount } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+  setup(){
+    const store = useStore();
+
+    onBeforeMount(()=>{
+      store.dispatch('fetchUser');
+    })
+  }
+}
+</script>
 
 <style>
 #app {
