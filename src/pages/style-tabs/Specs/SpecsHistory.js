5347f321018c909db788d14026d71f90c43e07b9
ksondjaja <sondjajaja@gmail.com>
2024-04-29 18:08:25 -0400

    edit & delete spec rows

--- src/pages/style-tabs/Specs/SpecsHistory.js
+++ src/pages/style-tabs/Specs/SpecsHistory.js
@@ -1,9 +1,4 @@
-// Tutorial on loading from database into Data Grid:
-// https://www.freecodecamp.org/news/how-to-integrate-material-ui-data-grid-in-react-using-data-from-a-rest-api/
-// Save row edit:
-// https://stackoverflow.com/questions/69843397/how-to-get-values-of-row-after-edit-for-mui-data-grid
-
-// Need to debug why updated row id is undefined
+// MUI DATA GRID DOCUMENTATION: https://mui.com/x/react-data-grid/editing/
 
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
@@ -18,41 +13,70 @@ import {
     Typography,
     Button
 } from '@mui/material';
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import { 
+  DataGrid,
+  GridColDef,
+  GridActionsCellItem,
+  GridRowId,
+  GridRowsProp
+} from '@mui/x-data-grid';
 
 
 export default function SpecsHistory ( props ){
 
-  const [tableData, setTableData] = useState([])
+  const [tableData, setTableData] = useState([
+    // {
+    //   id: 1,
+    //   code: 'T101',
+    //   pom: 'Front Body Length - from HPS Seam',
+    //   init: 24.5
+    // },
+    // {
+    //   id: 2,
+    //   code: 'T102',
+    //   pom: 'Back Body Length - from HPS Seam',
+    //   init: 24
+    // },
+    // {
+    //   id: 3,
+    //   code: 'T103',
+    //   pom: 'Shoulder Width - seam to seam',
+    //   init: 15
+    // }
+  ])
+
   const [rowCount, setRowCount] = useState(tableData.length)
-  const [error, setError] = useState()
+  //const [error, setError] = useState()
 
 
-  const handleProcessRowUpdate = (updatedRow, originalRow) => {
-    console.log(tableData)
+  const saveUpdatedRow = (updatedRow) => {
 
-    console.log(updatedRow);
+    const rowId = parseInt(updatedRow.id);
 
-    const updatedId = parseInt(updatedRow["id"]);
+    //console.log(rowId);
 
-    console.log(updatedId);
+    let newTable = tableData;
 
-    let newTable = [tableData.items];
+    newTable[rowId-1] = updatedRow;
 
-    newTable[updatedId-1] = updatedRow;
     setTableData(newTable);
 
-    console.log(tableData)
+    return(updatedRow)
   }
 
-  const handleProcessRowUpdateError = error => {
-    setError(error.message)
-  }
+  // const handleProcessRowUpdateError = error => {
+  //   setError(error.message)
+  // }
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    setTableData(tableData.filter((row) => row.id !== id));
+  };
   
   const handleAddPOM = () => {
 
     const newPOM = {
-      id: rowCount+1,
+      id: parseInt(rowCount+1),
       code: '',
       pom: '',
       init: 0
@@ -65,53 +89,32 @@ export default function SpecsHistory ( props ){
     setRowCount(rowCount+1)
   }
 
-    return(
-        <Grid container spacing={2}>
-            <Grid item xs={12}>
-              {error}
-            </Grid>
-            <Grid item xs={6}>
-                <DataGrid rows={tableData} columns={initColumns}
-                  processRowUpdate={handleProcessRowUpdate}
-                  onProcessRowUpdateError={handleProcessRowUpdateError}
-                  autoHeight={true}
-                  sx={{'.MuiDataGrid-cell': { borderRight: '1px solid #d0d0d0' },
-                  '.MuiDataGrid-footerContainer': { display: 'none' },
-                  "& .MuiDataGrid-columnHeaderTitle": {
-                    whiteSpace: "normal",
-                    lineHeight: "normal"
-                  },
-                  "& .MuiDataGrid-columnHeader": {
-                    // Forced to use important since overriding inline styles
-                    height: "unset !important"
-                  },
-                  "& .MuiDataGrid-columnHeaders": {
-                    // Forced to use important since overriding inline styles
-                    maxHeight: "168px !important"}}}
-                />
-            </Grid>
-            <Grid item xs={6}>
-
-            </Grid>
-            <Grid item xs={12}>
-              <Button color="primary" variant="contained" onClick={handleAddPOM}>
-                Add POM
-              </Button>
-            </Grid>
-
-        </Grid>
-    )
-}
-
-const initColumns: GridColDef[] = [
+  const initColumns: GridColDef[] = [
     {
-        field: 'code',
-        headerName: 'Code',
-        editable: true,
-        align: 'left',
-        headerAlign: 'left',
-        width: 120,
-        columnMenuAlign: 'left'
+      field: 'delete',
+      type: 'actions',
+      headerName: '',
+      align: 'left',
+      disableColumnMenu: true,
+      renderCell: ({ id }) => {
+        return(
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />
+        )
+      }
+    },
+    {
+      field: 'code',
+      headerName: 'Code',
+      editable: true,
+      align: 'left',
+      headerAlign: 'left',
+      width: 120,
+      columnMenuAlign: 'left'
     },
     { 
       field: 'pom',
@@ -150,6 +153,45 @@ const initColumns: GridColDef[] = [
       editable: false,
     },
   ]
+
+    return(
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+                <DataGrid rows={tableData} columns={initColumns}
+                  processRowUpdate={(updatedRow, originalRow)=>{
+                    return saveUpdatedRow(updatedRow)
+                  }}
+                  //onProcessRowUpdateError={handleProcessRowUpdateError}
+                  autoHeight={true}
+                  sx={{'.MuiDataGrid-cell': { borderRight: '1px solid #d0d0d0' },
+                  '.MuiDataGrid-footerContainer': { display: 'none' },
+                  "& .MuiDataGrid-columnHeaderTitle": {
+                    whiteSpace: "normal",
+                    lineHeight: "normal"
+                  },
+                  "& .MuiDataGrid-columnHeader": {
+                    // Forced to use important since overriding inline styles
+                    height: "unset !important"
+                  },
+                  "& .MuiDataGrid-columnHeaders": {
+                    // Forced to use important since overriding inline styles
+                    maxHeight: "168px !important"}}}
+                />
+            </Grid>
+            <Grid item xs={6}>
+
+            </Grid>
+            <Grid item xs={12}>
+              <Button color="primary" variant="contained" onClick={handleAddPOM}>
+                Add POM
+              </Button>
+            </Grid>
+
+        </Grid>
+    )
+}
+
+
 
   // const initRows: GridRowsProp = [
   //   {
